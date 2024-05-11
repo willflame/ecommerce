b14a69fb826f600049c0772cb2317e19544c7a63
Willian Anselmo da Silva (Megazordy) <willian0506@hotmail.com>
2024-05-11 16:42:04 -0300

    feat (layout): integrates header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+
+import { LayoutModule } from '@ecommerce/layout';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule, LayoutModule],
   selector: 'ecommerce-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
