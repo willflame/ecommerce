f3be2c1c89a8a4628d18ec676710d8dc52e9c2a4
Willian Anselmo da Silva (Megazordy) <willian0506@hotmail.com>
2024-05-28 21:51:04 -0300

    feat: add angular material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,12 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { appRoutes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes), provideHttpClient()],
+  providers: [
+    provideRouter(appRoutes),
+    provideHttpClient(),
+    provideAnimationsAsync(),
+  ],
 };
