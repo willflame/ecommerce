a6016a850cad0f293e84a092a7626f7594d766c0
Willian Anselmo da Silva (Megazordy) <willian0506@hotmail.com>
2024-05-29 13:38:25 -0300

    feat (product-search): integrates api

--- modules/data-access/product/src/index.ts
+++ modules/data-access/product/src/index.ts
@@ -1,2 +1,3 @@
 export * from './lib/mocks/products.mock';
+export * from './lib/models/product.model';
 export * from './lib/services/product-search/product-search.service';
--- modules/feature/product/search/src/lib/product-search/product-search.component.ts
+++ modules/feature/product/search/src/lib/product-search/product-search.component.ts
@@ -1,12 +1,19 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
 // eslint-disable-next-line @nx/enforce-module-boundaries
-import { mockProducts } from '@ecommerce/product-data-access';
+import { Product, ProductSearchService } from '@ecommerce/product-data-access';
+import {
+  Observable,
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  switchMap,
+} from 'rxjs';
 
 @Component({
   selector: 'ecommerce-product-search',
@@ -21,7 +28,18 @@ import { mockProducts } from '@ecommerce/product-data-access';
   templateUrl: './product-search.component.html',
   styleUrl: './product-search.component.scss',
 })
-export class ProductSearchComponent {
-  control = new FormControl('');
-  products = mockProducts;
+export class ProductSearchComponent implements OnInit {
+  control = new FormControl('', { nonNullable: true });
+  products$!: Observable<Product[]>;
+
+  constructor(private _productSearchService: ProductSearchService) {}
+
+  ngOnInit(): void {
+    this.products$ = this.control.valueChanges.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      filter((text) => text?.length > 2),
+      switchMap((text) => this._productSearchService.searchByName(text))
+    );
+  }
 }
